3ece2ba0e6302815378568ff2700b7f455abc75f
Cristhian-Chavez <crsth.1990@fpuna.edu.py>
2023-02-01 04:12:42 -0300

    conditional render

--- src/App.js
+++ src/App.js
@@ -5,17 +5,41 @@ import posts from './posts'
 // Modifica el componente App para implmentar la funcionalidad requerida
 
 class App extends Component {
+    constructor() {
+        super()
+        this.state = {
+            strValue: '',
+        }
+    }
+    handleChange = (e) => {
+        const value = e.target.value;
+        this.setState( () => {
+            return{ strValue: value}
+        })
+    }
   render() {
     return (
       <div>
         <div className="filter">
-          <input type="text" placeholder="Ingresa el término de búsqueda" />
+          <input
+              type="text"
+              placeholder="Ingresa el término de búsqueda"
+              onChange={ this.handleChange }
+              value={ this.state.strValue }
+          />
         </div>
         <ul>
-          <li>
-            <a href={posts[0].url}><img src={posts[0].image } /></a>
-            <p>{ posts[0].title }</p>
-          </li>
+            {
+                this.state.strValue.length >= 1 ? "No hay" :
+                posts.map( (el, i) => {
+                    return(
+                        <li key={ i }>
+                            <a href={el.url}><img src={el.image}/></a>
+                            <p>{el.title}</p>
+                        </li>
+                    )
+                })
+            }
         </ul>
       </div>
     )
